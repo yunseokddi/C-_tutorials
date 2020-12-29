6aa464ef518f15024f133501c88a47aabb746709
yunseokddi <oys5339@naver.com>
2020-12-29 10:56:26 +0900

    Concole App tutorial finish

---
+++ Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+    }
+}
---
+++ LocalTutorials/LocalTutorials/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LocalTutorials
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+
+            int c = a + b;
+            Console.WriteLine(c);
+
+            c = a - b;
+            Console.WriteLine(c);
+
+            c = a * b;
+            Console.WriteLine(c);
+
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void Main(string[] args)
+        {
+            //WorkingWithIntegers();
+        }
+    }
+}
