8fe3f607c123dc9e962dd58c866d4cd51d94e137
yunseokddi <oys5339@naver.com>
2021-01-06 13:58:49 +0900

    WSP tutorial start

---
+++ WPF_Tutorials/WPF_Tutorials/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Tutorials
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_owner_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = new ChildWindow();
+            win.Owner = this;
+            win.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            win.Show();
+        }
+
+        private void btn_screen_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = new ChildWindow();
+            win.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            win.Show();
+        }
+
+        private void btn_manual_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = new ChildWindow();
+            win.WindowStartupLocation = WindowStartupLocation.Manual;
+            win.Show();
+        }
+
+        private void btn_top_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = new ChildWindow();
+            win.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            win.Topmost = true;
+            win.Background = Brushes.Cyan;
+            win.Show();
+        }
+    }
+}
---
+++ dot_net_tutorial/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+        private int maxSpeed;
+        private int speed = 0;
+        private string model;
+
+        public Car(int maxSpeed, string model)
+        {
+            this.maxSpeed = maxSpeed;
+            this.model = model;
+        }
+
+        public void ShowCarInformation()
+        {
+            Console.WriteLine(model + "의 현재 속도: " + speed + "km/h, 최대 속도: " + maxSpeed + "km/h");
+
+        }
+        public void speedUp(int increment)
+        {
+            if (speed + increment > maxSpeed)
+                Console.WriteLine("최대 속도 " + maxSpeed + "km/h를 넘길 수 없습니다.");
+            else
+            {
+                speed += increment;
+                Console.WriteLine(model + "의 현재 속도는 " + speed + "km/h 입니다.");
+            }
+        }
+
+        public void speedDown(int decrement)
+        {
+            if (speed - decrement < 0)
+                Console.WriteLine("속도는 0 아래로 떨어질 수 없습니다.");
+            else
+            {
+                speed -= decrement;
+                Console.WriteLine(model + "의 현재 속도는 " + speed + "km/h 입니다.");
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(325, "Audi");
+
+            car.ShowCarInformation();
+            car.speedUp(50);
+            car.speedUp(40);
+            car.speedUp(210);
+            car.speedUp(30);
+        }
+    }
+}
